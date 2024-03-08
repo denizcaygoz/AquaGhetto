3f7d87a7aecde1f198a932db1022d94c3d006c5e
Tristan <tristanst.04@gmail.com>
2024-03-08 13:51:03 +0100

    Implemented checkBabyPrisoner

--- src/main/kotlin/service/PlayerActionService.kt
+++ src/main/kotlin/service/PlayerActionService.kt
@@ -2,6 +2,7 @@ package service
 
 import entity.Player
 import entity.PrisonBus
+import entity.enums.PrisonerTrait
 import entity.enums.PrisonerType
 import entity.tileTypes.PrisonerTile
 import entity.tileTypes.Tile
@@ -44,6 +45,38 @@ class PlayerActionService(private val rootService: RootService): AbstractRefresh
     }
 
     fun checkBabyPrisoner(): PrisonerType? {
+        val game = rootService.currentGame
+        checkNotNull(game) { "No game started yet." }
+
+        /*get breedable prisoners*/
+        val foundBreedableMale = mutableMapOf<PrisonerType, PrisonerTile>()
+        val foundBreedableFemale = mutableMapOf<PrisonerType, PrisonerTile>()
+        val player = game.players[game.currentPlayer]
+        val board = player.board
+        for (entry1 in board.getPrisonYardIterator()) {
+            val secondMap = entry1.value
+            for (entry2 in secondMap) {
+                val tile = entry2.value
+                if (tile !is PrisonerTile) continue
+                val trait = tile.prisonerTrait
+                val type = tile.prisonerType
+                if (trait == PrisonerTrait.MALE || tile.breedable) foundBreedableMale[type] = tile
+                if (trait == PrisonerTrait.FEMALE || tile.breedable) foundBreedableFemale[type] = tile
+            }
+        }
+
+        /*check for breedable prisoners*/
+        for (type in PrisonerType.values()) {
+            val male: PrisonerTile? = foundBreedableMale[type]
+            val female: PrisonerTile? = foundBreedableMale[type]
+            if (male != null && female != null) {
+                male.breedable = false
+                female.breedable = false
+                return type
+            }
+        }
+
+        /*no breedable prisoner was found*/
         return null
     }
 
