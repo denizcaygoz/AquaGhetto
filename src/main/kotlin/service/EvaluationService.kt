df1a55e677c5bce2ce1114e8574c337212d17f3c
Tristan <tristanst.04@gmail.com>
2024-03-09 22:26:05 +0100

    Implemented getPrisonerTypeCount

--- src/main/kotlin/service/EvaluationService.kt
+++ src/main/kotlin/service/EvaluationService.kt
@@ -2,6 +2,7 @@ package service
 
 import entity.Player
 import entity.enums.PrisonerType
+import entity.tileTypes.PrisonerTile
 
 class EvaluationService(private val rootService: RootService): AbstractRefreshingService() {
 
@@ -13,8 +14,25 @@ class EvaluationService(private val rootService: RootService): AbstractRefreshin
 
     }
 
+    /**
+     * Creates a map containing the amount of different PrisonerTypes a player owns
+     *
+     * @param player the player whose map should be created
+     * @return the map containing the amount of prisoners
+     */
     fun getPrisonerTypeCount(player: Player): MutableMap<PrisonerType, Int> {
-        return mutableMapOf()
+        val resultMap = mutableMapOf<PrisonerType, Int>()
+
+        for (xIterator in player.board.getPrisonYardIterator()) {
+            for (yIterator in xIterator.value) {
+                val tile = yIterator.value
+                if (tile !is PrisonerTile) continue
+                val current = resultMap[tile.prisonerType] ?: 0
+                resultMap[tile.prisonerType] = current + 1
+            }
+        }
+
+        return resultMap
     }
 
 }
