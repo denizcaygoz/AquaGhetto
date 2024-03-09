13c7969f6b5677994d5d55f3e55fcd991cd048b8
Tristan <tristanst.04@gmail.com>
2024-03-09 19:08:35 +0100

    Implemented startNewRound in GameService

--- src/main/kotlin/service/GameService.kt
+++ src/main/kotlin/service/GameService.kt
@@ -7,6 +7,17 @@ import view.Refreshable
 
 class GameService(private val rootService: RootService): AbstractRefreshingService() {
 
+    /**
+     * Starts a new game
+     *
+     * Function used to start a new game by creating a new instance of AquaGhetto
+     * Creates a list of players bases on the argument and sets the list of players in AquaGhetto to
+     * this list. Every board of a player is initialized with the default prison yard.
+     * The list of all tiles is created and the normal drawStack and the finalStack is created
+     * The list of prison buses is also created
+     * At the end rootService.currentGame is set to the newly created game
+     * refreshAfterStartGame and refreshAfterNextTurn with the current player is called
+     */
     fun startNewGame(players: MutableList<Pair<String, PlayerType>>) {
 
         /*checks for a valid player count*/
@@ -49,6 +60,70 @@ class GameService(private val rootService: RootService): AbstractRefreshingServi
         /*Call refreshes*/
         onAllRefreshables {
             refreshAfterStartGame()
+            refreshAfterNextTurn(game.players[game.currentPlayer])
+        }
+    }
+
+    /**
+     * Function to determine the next player
+     *
+     * This function is used to set game.currentPlayer to the "next" player based on the amount of
+     * taken buses
+     * If a card was taken from the finalStack and all players have taken a
+     * bus the game will end, evaluateGame is called
+     */
+    fun determineNextPlayer() {
+        val game = rootService.currentGame
+        checkNotNull(game) { "No running game." }
+        var amountTakenBusses = 0
+        for (player in game.players) {
+            if (player.takenBus == null) {
+                amountTakenBusses++
+            }
+        }
+
+        when (amountTakenBusses) {
+            1 ->  {
+                onAllRefreshables {
+                    refreshAfterNextTurn(game.players[game.currentPlayer])
+                }
+            }
+            0 -> { /*all players have taken a buss*/
+                if (game.finalStack.size != 15) { /*reserve stack was taken*/
+                    rootService.evaluationService.evaluateGame()
+                } else {
+                    /*reserve stack was not taken*/
+                    /*next player is the current player*/
+                    startNewRound(game)
+                }
+            }
+            else -> { /*sets the current player to the next player*/
+                game.currentPlayer = (game.currentPlayer + 1) % game.players.size
+                onAllRefreshables {
+                    refreshAfterNextTurn(game.players[game.currentPlayer])
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Function to start a new round
+     *
+     * This function is used to start a new round, all prisonBusses
+     * are placed in the middle
+     */
+    private fun startNewRound(game: AquaGhetto) {
+        for (player in game.players) {
+            val bus = player.takenBus
+            checkNotNull(bus) {"Not all players have taken a bus"}
+            game.prisonBusses.add(bus)
+            onAllRefreshables {
+                refreshPrisonBus(bus)
+            }
+            player.takenBus = null
+        }
+        onAllRefreshables {
             refreshAfterNextTurn(game.players[game.currentPlayer])
         }
     }
