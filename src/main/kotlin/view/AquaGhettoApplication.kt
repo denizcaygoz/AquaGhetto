c682ad54a31eee2f8f65a81d2d45d5d2c807022c
Nicolas <nicolas.mateischek@tu-dortmund.de>
2024-03-11 23:05:02 +0100

    Application V.2
    Finished implementation Missing Scenes to work

--- src/main/kotlin/view/AquaGhettoApplication.kt
+++ src/main/kotlin/view/AquaGhettoApplication.kt
@@ -7,60 +7,92 @@ import tools.aqua.bgw.core.BoardGameApplication
  * Application and SceneManagement
  * Needs to be adjusted Down the Line if something is missing
  */
-class AquaGhettoApplication: BoardGameApplication("AquaGhetto"), Refreshable
+class AquaGhettoApplication: BoardGameApplication("AquaGhetto"), Refreshable {
 
-/**
- * Central Service to provide all Game/PlayerActions
- */
-private val rootService = RootService()
+    /**
+     * Central Service to provide all Game/PlayerActions
+     */
+    private val rootService = RootService()
 
-/**
- * Main Ingame Scene
- * this will need a Esc Key Listener to go to Pause Menu
- */
-private val inGameScene = InGameScene(rootService).apply {
-    onKeyPressed = {
-        if (it.keyCode.isEscape) { this@AquaGhettoApplication.showMenuScene(pauseMenuScene)}
-    }
-}
-
-/**
- * The Main Menu Screen for selecting a mode
- */
-private val mainMenuScene = MainMenuScene(rootService).apply {
-
-    quitButton.onMouseClicked = { exit() }
-
-    //This needs to refrence the Name of the Button on the MainMenuScene that starts a new Game
-    newSingleplayerGameButton.onMouseClicked = {
-        this@AquaGhettoApplication.showMenuScene(setupScene)
+    /**
+     * Main in game Scene
+     * this will need an Esc Key Listener to go to Pause Menu
+     */
+    private val inGameScene = InGameScene(rootService).apply {
+        onKeyPressed = {
+            if (it.keyCode.isEscape) { this@AquaGhettoApplication.showMenuScene(pauseMenuScene)}
+        }
     }
 
-    /*this is a Placeholder for a Button that starts or Joins a Multiplayer Game
-    For the Time being the Multiplayer Button will refer to Single Player Setup */
-    newMultiplayerGameButton.onMouseClicked = {
-        this@AquaGhettoApplication.showMenuScene(setupScene)
+    /**
+     * The Main Menu Screen for selecting a mode
+     */
+    private val mainMenuScene = MainMenuScene(rootService).apply {
+
+        quitButton.onMouseClicked = { exit() }
+
+        //This needs to reference the Name of the Button on the MainMenuScene that starts a new Game
+        newSingleplayerGameButton.onMouseClicked = {
+            this@AquaGhettoApplication.showMenuScene(setupScene)
+        }
+
+        /*this is a Placeholder for a Button that starts or Joins a Multiplayer Game
+        For the Time being the Multiplayer Button will refer to Single Player Setup */
+        newMultiplayerGameButton.onMouseClicked = {
+            this@AquaGhettoApplication.showMenuScene(setupScene)
+        }
+    }
+
+    /**
+     * The PausePage that opens up by pressing Esc while being in the In game
+     */
+    private val pauseMenuScene = PauseMenuScene(rootService).apply {
+        onKeyPressed = {
+            if (it.keyCode.isEscape) { this@AquaGhettoApplication.hideMenuScene(pauseMenuScene)}
+        }
+        //maybe this needs to be coded in the scene and not here?
+        undoButton.onMouseClicked = { rootService.gameStatesService.undo() }
+        redoButton.onMouseClicked = { rootService.gameStatesService.redo() }
+        saveButton.onMouseClicked = { rootService.gameStatesService.saveGame() }
+        loadButton.onMouseClicked = { rootService.gameStatesService.loadGame() }
+    }
+
+    /**
+     * The Final Scene After the Game that shows the Scoreboard of Players
+     */
+    private val scoreboardScene = ScoreboardScene(rootService).apply {
+        backToMenuButton.onMouseClicked = {
+            this@AquaGhettoApplication.hideMenuScene()
+            this@AquaGhettoApplication.showMenuScene(mainMenuScene)
+        }
+    }
+
+    /**
+     * The Scene for setting up a new Game
+     */
+    private val setupScene = SetupScene(rootService)
+
+    init{
+        //we dont have a "addRefreshables" function so everything needs to be added individually
+        rootService.addRefreshable(this)
+        rootService.addRefreshable(inGameScene)
+        rootService.addRefreshable(mainMenuScene)
+        rootService.addRefreshable(pauseMenuScene)
+        rootService.addRefreshable(scoreboardScene)
+        rootService.addRefreshable(setupScene)
+
+        //this is from NetWar
+        onWindowClosed = {
+            rootService.networkService.disconnect()
+        }
+        this.showMenuScene(mainMenuScene)
+    }
+
+    override fun refreshAfterStartGame() {
+        this.hideMenuScene()
+    }
+
+    override fun refreshAfterEndGame() {
+        this.showMenuScene(scoreboardScene)
     }
 }
-
-/**
- * The PausePage that opens up by pressing Esc while being in the Ingame
- */
-private val pauseMenuScene = PauseMenuScene(rootService).apply {
-
-    //maybe this needs to be coded in the scene and not here?
-    undoButton.onMouseClicked = { rootService.gameStatesService.undo() }
-    redoButton.onMouseClicked = { rootService.gameStatesService.redo() }
-    saveButton.onMouseClicked = { rootService.gameStatesService.saveGame() }
-    loadButton.onMouseClicked = { rootService.gameStatesService.loadGame() }
-}
-
-/**
- * The Final Scene After the Game that shows the Scoreboard of Players
- */
-private val scoreboardScene = ScoreboardScene(rootService)
-
-/**
- * The Scene for setting up a new Game
- */
-private val setupScene = SetupScene(rootService)
