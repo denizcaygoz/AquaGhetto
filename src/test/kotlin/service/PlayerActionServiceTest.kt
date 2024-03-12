788fe2b5aef27ddef54e2130096f15a3928cf384
PhilippL <philipp.lachowski@tu-dortmund.de>
2024-03-12 11:09:32 +0100

    MoveEmployee tests done

--- src/test/kotlin/service/PlayerActionServiceTest.kt
+++ src/test/kotlin/service/PlayerActionServiceTest.kt
@@ -1,6 +1,7 @@
 package service
 
 import entity.enums.PlayerType
+import entity.tileTypes.GuardTile
 import kotlin.test.*
 
 class PlayerActionServiceTest {
@@ -73,5 +74,64 @@ class PlayerActionServiceTest {
         }
         assertSame(1,currentPlayer.coins)
         assertSame(2, currentPlayer.remainingSmallExtensions)
+    }
+
+    @Test
+    fun `move employee from source to destination within prison yard`() {
+        val game = rootService.currentGame!!
+        val currentPlayer = game.players[game.currentPlayer]
+        currentPlayer.coins = 1
+        currentPlayer.board.setPrisonYard(1, 1, GuardTile()) // Place a guard tile at (1,1)
+
+        rootService.playerActionService.moveEmployee(1,1,2,2)
+
+        assertTrue(currentPlayer.board.getPrisonYard(2, 2) is GuardTile)
+        assertEquals(0, currentPlayer.coins) // Check coins decremented
+    }
+
+    @Test
+    fun `move notGuard from source to empty destination`() {
+        val game = rootService.currentGame!!
+        val currentPlayer = game.players[game.currentPlayer]
+        currentPlayer.hasJanitor = true
+        currentPlayer.coins = 1 // Set up a janitor
+
+        rootService.playerActionService.moveEmployee(-102, -102, 3, 3)
+
+        assertFalse(currentPlayer.hasJanitor)
+        assertEquals(0, currentPlayer.coins) // Check coins decremented
+    }
+
+    @Test
+    fun `move Guard to special destination`() {
+        val game = rootService.currentGame!!
+        val currentPlayer = game.players[game.currentPlayer]
+        currentPlayer.board.setPrisonYard(3, 3, GuardTile()) // Place a guard tile at (3,3)
+        currentPlayer.coins = 1 // Set up coins for placement
+
+        rootService.playerActionService.moveEmployee(3, 3, -103, -103)
+
+        assertFalse(currentPlayer.board.getPrisonYard(3, 3) is GuardTile)
+        assertTrue(currentPlayer.secretaryCount == 1) // Check janitor removed
+        assertEquals(0, currentPlayer.coins) // Check coins decremented
+    }
+    @Test
+    fun `move employee to special destination without enough coins`() {
+        val game = rootService.currentGame!!
+        val currentPlayer = game.players[game.currentPlayer]
+        currentPlayer.board.setPrisonYard(3, 3, GuardTile()) // Place a guard tile at (3,3)
+        currentPlayer.coins = 0
+
+        assertFails{rootService.playerActionService.moveEmployee(3, 3, -103, -103)}
+    }
+
+    @Test
+    fun `move employee to full destination`() {
+        val game = rootService.currentGame!!
+        val currentPlayer = game.players[game.currentPlayer]
+        currentPlayer.secretaryCount = 2 // Maximum secretaries already
+        currentPlayer.coins = 1
+
+        assertFails{rootService.playerActionService.moveEmployee(3, 3, -103, -103)}
     }
 }
