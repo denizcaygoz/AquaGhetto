7b30182569f9df5740b7d4090531baca1f50d515
Tristan <tristanst.04@gmail.com>
2024-03-12 10:59:57 +0100

    Small fix in ValidationService

--- src/main/kotlin/service/ValidationService.kt
+++ src/main/kotlin/service/ValidationService.kt
@@ -35,15 +35,14 @@ class ValidationService(private val rootService: RootService): AbstractRefreshin
 
         /*check if there is no other prisonerType around*/
         /*check if there is a prisoner of the same type around*/
+        val toCheckSurrounding = mutableListOf(Pair(1,0),Pair(-1,0),Pair(0,1),Pair(0,-1))
         var surroundingSameType = false
-        for (xIterate in -1..1) {
-            for (yIterate in -1..1) {
-                val tileCheck = board.getPrisonYard(x + xIterate, y + yIterate)
-                if (tileCheck is PrisonerTile && (tileCheck.prisonerType != tile.prisonerType)) {
-                    return false
-                } else if (tileCheck is PrisonerTile && xIterate != 0 && yIterate != 0) {
-                    surroundingSameType = true
-                }
+        for (offset in toCheckSurrounding) {
+            val tileCheck = board.getPrisonYard(x + offset.first, y + offset.second)
+            if (tileCheck is PrisonerTile && (tileCheck.prisonerType != tile.prisonerType)) {
+                return false
+            } else if (tileCheck is PrisonerTile && offset.first != 0 && offset.second != 0) {
+                surroundingSameType = true
             }
         }
 
@@ -150,15 +149,18 @@ class ValidationService(private val rootService: RootService): AbstractRefreshin
         return true
     }
 
+    /**
+     * checks whether there is a grid surrounding the supplied location
+     *
+     * @param board the board in which the check is performed
+     * @param location the location of the intended new grid
+     * @return if the location is surrounded by a grid
+     */
     private fun checkIfSurroundedByOtherGrid(board: Board, location: Pair<Int, Int>): Boolean {
-        for (xIterate in -1..1) {
-            for (yIterate in -1..1) {
-                if (board.getPrisonGrid(location.first + xIterate, location.second + yIterate)) {
-                    return true
-                }
-            }
-        }
-        return false
+        return board.getPrisonGrid(location.first + 1, location.second + 0) ||
+                board.getPrisonGrid(location.first - 1, location.second + 0) ||
+                board.getPrisonGrid(location.first + 0, location.second + 1) ||
+                board.getPrisonGrid(location.first + 0, location.second - 1)
     }
 
 }
