2a6b73dde3c4d4969f679b007576be0fc9df48e2
malek.briki <malek.briki@tu-dortmund.de>
2024-03-14 22:54:42 +0100

    GameStatesServiceTest without Valid save and load

---
+++ src/test/kotlin/service/GameStatesServiceTest.kt
@@ -0,0 +1,95 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.*
+import entity.enums.PlayerType
+import org.junit.jupiter.api.assertThrows
+import service.*
+
+class GameStatesServiceTest {
+    val rootService = RootService()
+        @Test
+        fun loadInvalidGameTest(){
+            // Test when no saved file exists
+            val game1 = assertThrows<IllegalStateException> { rootService.gameStatesService.loadGame() }
+            assertEquals( "There is no game to load", game1.message)
+        }
+
+    //TODO loadValidGameTest
+
+    @Test
+    fun saveInvalidGameTest(){
+        // Test when no saved file exists
+        val message1 = assertThrows<IllegalStateException> { rootService.gameStatesService.saveGame() }
+        assertEquals( "No game to save", message1.message)
+    }
+
+    //TODO saveValidGameTest
+
+    @Test
+    fun undoInvalidGameState(){
+        val players = mutableListOf(
+                Pair("P1", PlayerType.PLAYER),
+                Pair("P2", PlayerType.PLAYER)
+            )
+            rootService.gameService.startNewGame(players)
+        val message1 = assertThrows<IllegalStateException> { rootService.gameStatesService.undo() }
+        assertEquals( "Nothing to undo" , message1.message)
+    }
+
+    @Test
+    fun undoValidGameState() {
+        val players = mutableListOf(
+            Pair("P1", PlayerType.PLAYER),
+            Pair("P2", PlayerType.PLAYER)
+        )
+        rootService.gameService.startNewGame(players)
+        val firstState = rootService.currentGame
+        firstState!!.players[firstState.currentPlayer] = firstState.players.last()
+
+        firstState.previousState = firstState
+
+        rootService.gameStatesService.undo()
+
+        assertNotNull(rootService.currentGame!!.previousState)
+        assertEquals(
+            rootService.currentGame!!.players.last(),
+            rootService.currentGame!!.players[rootService.currentGame!!.currentPlayer]
+        )
+
+    }
+
+    @Test
+    fun redoInvalidGameState(){
+        val players = mutableListOf(
+            Pair("P1", PlayerType.PLAYER),
+            Pair("P2", PlayerType.PLAYER)
+        )
+        rootService.gameService.startNewGame(players)
+        val message1 = assertThrows<IllegalStateException> { rootService.gameStatesService.redo() }
+        assertEquals( "Nothing to redo" , message1.message)
+    }
+
+    @Test
+    fun redoValidGameState() {
+        val players = mutableListOf(
+            Pair("P1", PlayerType.PLAYER),
+            Pair("P2", PlayerType.PLAYER)
+        )
+        rootService.gameService.startNewGame(players)
+        val firstState = rootService.currentGame
+        firstState!!.players[firstState.currentPlayer] = firstState.players.last()
+
+        firstState.nextState = firstState
+
+        rootService.gameStatesService.redo()
+
+        assertNotNull(rootService.currentGame!!.nextState)
+        assertEquals(
+            rootService.currentGame!!.players.last(),
+            rootService.currentGame!!.players[rootService.currentGame!!.currentPlayer]
+        )
+
+    }
+
+
+
+}
