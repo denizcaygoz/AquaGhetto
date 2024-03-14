76e54c04539f53798c01f069fe36cf66bf43dfbe
Tristan <tristanst.04@gmail.com>
2024-03-14 14:07:29 +0100

    Changed maximize to int, to allow more than 2 players

--- src/main/kotlin/service/aiServices/smart/evaluateActions/EvaluateAddTileToPrisonBusService.kt
+++ src/main/kotlin/service/aiServices/smart/evaluateActions/EvaluateAddTileToPrisonBusService.kt
@@ -6,7 +6,7 @@ import service.aiServices.smart.SmartAI
 
 class EvaluateAddTileToPrisonBusService(val smartAI: SmartAI) {
 
-    fun getScoreAddTileToPrisonBus(game: AquaGhetto, depth: Int, maximize: Boolean, amountActions: Int): ActionAddTileToBus {
+    fun getScoreAddTileToPrisonBus(game: AquaGhetto, depth: Int, maximize: Int, amountActions: Int): ActionAddTileToBus {
         return ActionAddTileToBus(false, 0, 0)
     }
 
