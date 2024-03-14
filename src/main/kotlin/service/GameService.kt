10e7462d48eaf752504f34e1e44b317dc2705011
Cicéron Bradley Kulosa <ciceron.kulosa@udo.edu>
2024-03-14 10:49:53 +0100

    startNewGame checks if first player is AI

--- src/main/kotlin/service/GameService.kt
+++ src/main/kotlin/service/GameService.kt
@@ -71,6 +71,9 @@ class GameService(private val rootService: RootService): AbstractRefreshingServi
             refreshAfterStartGame()
             refreshAfterNextTurn(game.players[game.currentPlayer])
         }
+
+        /*if the first player is an AI call the AI service*/
+        this.checkAITurn(game.players[game.currentPlayer], 1500)
     }
 
     /**
