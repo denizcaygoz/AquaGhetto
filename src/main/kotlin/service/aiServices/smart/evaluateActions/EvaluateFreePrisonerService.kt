df751c7c0a479dea4a4bae769a8a5717d2e10cdf
Tristan <tristanst.04@gmail.com>
2024-03-14 17:35:15 +0100

    Implemented freePrisoner

--- src/main/kotlin/service/aiServices/smart/evaluateActions/EvaluateFreePrisonerService.kt
+++ src/main/kotlin/service/aiServices/smart/evaluateActions/EvaluateFreePrisonerService.kt
@@ -7,7 +7,16 @@ import service.aiServices.smart.SmartAI
 class EvaluateFreePrisonerService(val smartAI: SmartAI) {
 
     fun freePrisoner(game: AquaGhetto, depth: Int, maximize: Int, amountActions: Int): ActionFreePrisoner {
-        return ActionFreePrisoner(false, 0)
+        val player = game.players[game.currentPlayer]
+        if (player.coins < 2) {
+            return ActionFreePrisoner(false, 0)
+        }
+
+        val removedTile = player.isolation.pop()
+        val actionFree = smartAI.minMax(game, depth, maximize, amountActions)
+        player.isolation.add(removedTile)
+
+        return ActionFreePrisoner(actionFree.validAction, actionFree.score)
     }
 
 }
