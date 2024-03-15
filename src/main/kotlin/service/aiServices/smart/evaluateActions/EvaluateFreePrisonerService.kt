171f60907fd585ce39957f0040605ef0ce1ce305
dennismeister <dnzcgz008@gmail.com>
2024-03-15 17:32:18 +0100

    player coins by 2 decremented. And by 2 incremented.Comments added.

--- src/main/kotlin/service/aiServices/smart/evaluateActions/EvaluateFreePrisonerService.kt
+++ src/main/kotlin/service/aiServices/smart/evaluateActions/EvaluateFreePrisonerService.kt
@@ -4,16 +4,23 @@ import entity.AquaGhetto
 import entity.aIActions.ActionFreePrisoner
 import service.aiServices.smart.SmartAI
 
+/**
+ * This class simulates the free Prisoner action.
+ */
 class EvaluateFreePrisonerService(val smartAI: SmartAI) {
 
+    /**
+     * A function to simulate freePrisoner action.
+     */
     fun freePrisoner(game: AquaGhetto, depth: Int, maximize: Int, amountActions: Int): ActionFreePrisoner {
         val player = game.players[game.currentPlayer]
         if (player.coins < 2 || player.isolation.isEmpty()) {
             return ActionFreePrisoner(false, 0)
         }
-
+        player.coins -= 2
         val removedTile = player.isolation.pop()
         val actionFree = smartAI.minMax(game, depth, maximize, amountActions)
+        player.coins += 2
         player.isolation.add(removedTile)
 
         return ActionFreePrisoner(actionFree.validAction, actionFree.score)
