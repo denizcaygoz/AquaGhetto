0710224d633cd9ae8a3400075ac41d7f816e8aba
dennismeister <dnzcgz008@gmail.com>
2024-03-16 17:01:24 +0100

    freePrisoner fixed.

--- src/main/kotlin/service/aiServices/smart/evaluateActions/EvaluateFreePrisonerService.kt
+++ src/main/kotlin/service/aiServices/smart/evaluateActions/EvaluateFreePrisonerService.kt
@@ -1,7 +1,6 @@
 package service.aiServices.smart.evaluateActions
 
 import entity.AquaGhetto
-import entity.aIActions.AIAction
 import entity.aIActions.ActionFreePrisoner
 import service.aiServices.smart.SmartAI
 
@@ -9,9 +8,7 @@ class EvaluateFreePrisonerService(val smartAI: SmartAI) {
 
     fun freePrisoner(game: AquaGhetto, depth: Int, maximize: Int, amountActions: Int): ActionFreePrisoner {
         val player = game.players[game.currentPlayer]
-
-        //if player already took a bus, then the action cannot be done.
-        if (player.coins < 2 || player.isolation.isEmpty() || player.takenBus != null) {
+        if (player.coins < 2 || player.isolation.isEmpty()) {
             return ActionFreePrisoner(false, 0)
         }
 
@@ -19,18 +16,9 @@ class EvaluateFreePrisonerService(val smartAI: SmartAI) {
         player.coins -= 2
 
         val nextPlayer = smartAI.getNextAndOldPlayer(game,false)
+        game.currentPlayer = nextPlayer.second
 
-        val actionFree: AIAction
-
-        /*Player has the turn.*/
-        if(nextPlayer.second == game.currentPlayer) {
-            actionFree = smartAI.minMax(game, depth-1, 0, amountActions)
-        }
-        /*Enemy has the turn*/
-        else {
-            game.currentPlayer = nextPlayer.second
-            actionFree = smartAI.minMax(game, depth-1, 1, amountActions)
-        }
+        val actionFree = smartAI.minMax(game, depth, maximize, amountActions)
 
         game.currentPlayer = nextPlayer.first
         player.isolation.add(removedTile)
