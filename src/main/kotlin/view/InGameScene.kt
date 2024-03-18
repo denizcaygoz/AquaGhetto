65946383b604fabbad9db091bdf4820f659ac24a
Philipp Home PC <philipp.lachowski@tu-dortmund.de>
2024-03-18 09:44:11 +0100

    Bislang: clickbare tiles, eine Umrechnungsmethode für Coordinaten der Service Schicht in Coordinaten des Grids, eine Methode zum Schrumpfen der Grids falls sie größer werden, und Anfänge der StartNewGame Refresh Implementation

--- src/main/kotlin/view/InGameScene.kt
+++ src/main/kotlin/view/InGameScene.kt
@@ -1,5 +1,6 @@
 package view
 
+import entity.Player
 import entity.enums.PlayerType
 import service.RootService
 import tools.aqua.bgw.core.BoardGameApplication
@@ -14,17 +15,18 @@ import tools.aqua.bgw.components.uicomponents.Button
 import tools.aqua.bgw.visual.ColorVisual
 import tools.aqua.bgw.visual.Visual
 import java.awt.Color
+import kotlin.math.absoluteValue
 
-class InGameScene(rootService: RootService, test: SceneTest) : BoardGameScene(1920,1080), Refreshable {
+class InGameScene(var rootService: RootService, test: SceneTest) : BoardGameScene(1920,1080), Refreshable {
 
     private val testButton = Button(100,100,100,100)
     private val testButton2 = Button(100,100,100,100, visual = ColorVisual.PINK)
 
-    // Test Grid
-    private val testPrison = PlayerBoard()
+    // Grids
+    private val prisons : MutableList<PlayerBoard> = mutableListOf()
 
     // Camera Pane stuff
-    private val targetLayout = Pane<ComponentView>(width = 1920, height = 1080, visual = ImageVisual("Test_BackGround_Ingame.png"))
+    private val targetLayout = Pane<ComponentView>(width = 1920, height = 1080, visual = ColorVisual.RED)
     private val cameraPane = CameraPane(width = 1920, height = 1080, target = targetLayout, visual = ColorVisual.BLUE).apply {
         isHorizontalLocked = false
         isVerticalLocked = false
@@ -51,37 +53,87 @@ class InGameScene(rootService: RootService, test: SceneTest) : BoardGameScene(19
             }
         }
         targetLayout.addAll(
-            testButton,
-            testPrison
+            testButton
         )
 
         // Add the cameraPane to the scene
         addComponents(cameraPane)
     }
+
+    override fun refreshAfterStartGame() {
+        val game = rootService.currentGame
+        checkNotNull(game) {"There is no game running"}
+        val playerCount = game.players.size
+
+        for(i in 0 until playerCount) {
+            prisons.add(PlayerBoard(game.players[i]))
+        }
+
+        if (playerCount == 5) {
+            prisons[0].apply{
+                posX = 960.0
+                posY = 850.0
+            }
+            prisons[1].apply{
+                posX = 230.0
+                posY = 540.0
+            }
+            prisons[2].apply{
+                posX = 480.0
+                posY = 230.0
+            }
+            prisons[3].apply{
+                posX = 1440.0
+                posY = 230.0
+            }
+            prisons[4].apply{
+                posX = 1690.0
+                posY = 540.0
+            }
+        }
+        targetLayout.addAll(prisons)
+    }
 }
 
-class PlayerBoard() : GridPane<Button>(rows = 21, columns = 21, layoutFromCenter = false) {
+class PlayerBoard(player: Player) : GridPane<Button>(rows = 21, columns = 21, layoutFromCenter = true) {
 
     init {
         this.posX = 100.0
         this.posY = 100.0
+        this.spacing = 1.0
 
+        /**
         for (y in 0 until this.rows) {
             for (x in 0 until this.columns) {
-                val tempText : String = ""
-                this[x,y] = Button(height = 50, width = 50, text = "$y, $x", visual = Visual.EMPTY)
-                this.spacing = 1.0
-                //this[x,y].apply {on}
+                this[x,y] = Button(height = 50, width = 50, visual = Visual.EMPTY).apply {
+                    this.isDisabled = true
+                    this.isVisible = false
+                    //text = "$y, $x"
+                }
             }
         }
+        */
+
         for(y in 9..11) {
             for(x in 8..12) {
-                this[x,y]?.visual = ImageVisual("tiles/default_tile.png")
+                this[x,y] = Button(height = 50, width = 50, visual = Visual.EMPTY).apply{
+                    this.visual = ImageVisual("tiles/default_tile.png")
+                    this.isDisabled = false
+                    this.isVisible = true
+                }
             }
         }
         for(x in 9..11) {
-            this[x,8]?.visual = ImageVisual("tiles/default_tile.png")
-            this[x,12]?.visual = ImageVisual("tiles/default_tile.png")
+            this[x,8] = Button(height = 50, width = 50, visual = Visual.EMPTY).apply{
+                this.visual = ImageVisual("tiles/default_tile.png")
+                this.isDisabled = false
+                this.isVisible = true
+            }
+            this[x,12] = Button(height = 50, width = 50, visual = Visual.EMPTY).apply{
+                this.visual = ImageVisual("tiles/default_tile.png")
+                this.isDisabled = false
+                this.isVisible = true
+            }
         }
     }
 
@@ -92,6 +144,38 @@ class PlayerBoard() : GridPane<Button>(rows = 21, columns = 21, layoutFromCenter
     fun coords(serviceCoord : Int) : Int{
         return -(serviceCoord - 21)
     }
+
+    /**
+     * Returns the size, which the prison has to have on screen.
+     * 1.0 is full size
+     * 0.5 is half size and so on
+     */
+    fun calculateSize(player: Player) : Double {
+        // Save the span of the grid first
+        var maxX = -20
+        var minX = 20
+        var maxY = -20
+        var minY = 20
+        for ((x, innerMap) in player.board.getPrisonGridIterator()) {
+            for ((y, value) in innerMap) {
+                if (value) { // If the grid exists
+                    if (x < minX) minX = x
+                    if (y < minY) minY = y
+                    if (x > maxX) maxX = x
+                    if (y > maxY) maxY = y
+                }
+            }
+        }
+        val importantNumber =
+        if ((maxX.absoluteValue-minX.absoluteValue) >= (maxY.absoluteValue-minY.absoluteValue)) {
+            (maxX.absoluteValue-minX.absoluteValue)
+        } else (maxY.absoluteValue-minY.absoluteValue)
+
+        if(importantNumber <= 7) return 1.0
+        else if(importantNumber <= 9) return 0.8
+        else return 0.5
+
+    }
 }
 
 /**
@@ -100,6 +184,7 @@ class PlayerBoard() : GridPane<Button>(rows = 21, columns = 21, layoutFromCenter
 fun main() {
     val test = SceneTest()
     test.show()
+
 }
 
 class SceneTest : BoardGameApplication("AquaGhetto"), Refreshable {
@@ -107,8 +192,13 @@ class SceneTest : BoardGameApplication("AquaGhetto"), Refreshable {
     private val gameScene = InGameScene(rootService, this)
 
     init {
-        rootService.gameService.startNewGame(mutableListOf(Pair("Moin", PlayerType.PLAYER), Pair("Moin2", PlayerType.PLAYER)))
+        rootService.gameService.startNewGame(mutableListOf(
+            Pair("Moin0", PlayerType.PLAYER),
+            Pair("Moin1", PlayerType.PLAYER),
+            Pair("Moin2", PlayerType.PLAYER),
+            Pair("Moin3", PlayerType.PLAYER),
+            Pair("Moin4", PlayerType.PLAYER)))
+        gameScene.refreshAfterStartGame()
         showGameScene(gameScene)
-        show()
     }
 }
