72acfb97b0d9a1e55033c7123ff7cf59159bbb0a
Nicolas <nicolas.mateischek@tu-dortmund.de>
2024-03-20 11:11:08 +0100

    Delay input field Added
    is Missing Logic to make it Invisible

--- src/main/kotlin/view/SetupScene.kt
+++ src/main/kotlin/view/SetupScene.kt
@@ -2,6 +2,7 @@ package view
 
 import entity.Player
 import entity.enums.PlayerType
+import org.w3c.dom.Text
 import service.RootService
 import tools.aqua.bgw.components.layoutviews.GridPane
 import tools.aqua.bgw.components.uicomponents.*
@@ -33,233 +34,275 @@ class SetupScene (rootService : RootService, test: SceneTest2 = SceneTest2()) : 
      * CheckBox changes its state upon click
      *  ToggleButton might be suited better
      */
-    val testCheck = ToggleButton(posX = ((1920/2)-100), posY = (1080-200),text = "Normal Order", width = 400, ).apply {
-        onMouseClicked = {
-            //this.isSelected = !this.isSelected
-            if(this.isSelected){ text = "Random Order"}
-            else{ text = "Normal Order" }
+    val testCheck =
+        ToggleButton(posX = ((1920 / 2) - 100), posY = (1080 - 200), text = "Normal Order", width = 400,).apply {
+            onMouseClicked = {
+                //this.isSelected = !this.isSelected
+                if (this.isSelected) {
+                    text = "Random Order"
+                } else {
+                    text = "Normal Order"
+                }
+            }
         }
-    }
 
     /*Grid to Display PlayerCards*/
-    private val playerGrid: GridPane<UIComponent> = GridPane(posX = 600, posY =450, columns = 3, rows = 5, spacing = 25)
+    private val playerGrid: GridPane<UIComponent> =
+        GridPane(posX = 600, posY = 450, columns = 3, rows = 5, spacing = 25)
 
-    /**
-     * Creates a TextField were the User can Input a PlayerName
-     * Length of the input is unrestricted
-     * @param textInput String that gets put into text of the TextField
-     */
-    private fun createPlayerInputfield(textInput: String): TextField {
-        val textField = TextField(
-            posY = 0,
-            posX = 100,
-            width = 300,
-            height = 50,
-            font = Font(20 , Color.BLACK ),
-            text = textInput,
-        ).apply {
-            /*Check if Startnewgame should be disabled with every Keypress
-            * StartNewGameButton is Enabled from the Start so Going with Default Names works*/
-            onKeyTyped = {
-                if( determinePlayerCount() < 2 || determinePlayerCount() > 5 ) {
-                    startNewGameButton.isDisabled = true
-                }else{
-                    startNewGameButton.isDisabled = false
-                }
-                //Limit for InputLength could be added here
-            }
-        }
-
-        return textField
-    }
-
-    /**
-     * creates a Button that onCLick adds Inputfield TypeSelector and Remove Button to a Row in the grid
-     * does the Opposite as RemovePlayerButton
-     * @return Button
-     */
-    private fun createPlayerAddButton(position : Int) : Button {
-        val addButton = Button(
-            posX = 100,
-            posY = 0,
-            width = 50,
-            height = 50,
-            text = "add",
-            alignment = Alignment.CENTER,
-            //visual = ImageVisual("Test.png")
-        ).apply {
-            onMouseClicked = {
-                playerGrid[ 0 , position ] = createPlayerInputfield("Player${position+1}")
-                playerGrid[ 1 , position ] = createPlayerTypeSelector()
-                playerGrid[ 2 , position ] = createRemovePlayerButton(position)
-            }
-        }
-        return addButton
-    }
-
-    /**
-     * creates a button that will remove Player Creation Buttons from the grid at its position
-     * will also delete itself
-     * @return button
-     */
-    private fun createRemovePlayerButton(position: Int): Button {
-        val removeButton = Button(
-            posX = 0,
-            posY = 0    ,
-            width = 50,
-            height = 50,
-            text = "remove",
-            alignment = Alignment.CENTER_LEFT ,
-            //visual = ImageVisual("Test.png"),
-        ).apply {
-            onMouseClicked = {
-                //remove
-                playerGrid[ 0 , position ] = createPlayerAddButton(position)
-                playerGrid[ 1 , position ] = null
-                playerGrid[ 2 , position ] = null
-
-                if(determinePlayerCount() >= 2)
-                {
-                    startNewGameButton.isDisabled = true
-                }
-            }
-        }
-        return removeButton
-    }
-
-    /**
-     * creates a Button that can switch from one Playertype to another
-     * State is saved in text with Fontsize 0
-     * @return Button
-     */
-    private fun createPlayerTypeSelector() : Button {
-        val typeSelector = Button (
-            posX = 0,
-            posY = 0,
-            width = 50,
-            height = 50,
-            text = "PLAYER",
-            font = Font(size = 0),
-            alignment = Alignment.CENTER,
-            //visual = ImageVisual("PLAYER.png"),
-        ).apply {
-            /*Unsure if we should be able to swap a Player to a Network Player
-            * Network case can be removed in that case*/
-            onMouseClicked = {
-                when(text){
-                    "PLAYER" -> { text = "AI" ; visual = ImageVisual("AI.png") }
-                    "AI" -> { text = "RANDOM" ; visual = ImageVisual("RANDOM.png") }
-                    "RANDOM" -> { text = "NETWORK" ; visual = ImageVisual("NETWORK.png") }
-                    "NETWORK" -> { text = "PLAYER"; visual = ImageVisual("PLAYER.png") }
-                    else ->  { text = "PLAYER"; visual = ImageVisual("PLAYER.png") }
-                }
-            }
-        }
-        return typeSelector
-    }
-
-    /**This Button Needs
-     * a [getPlayerList] Function
-     * to validate amount of Players with Names [determinePlayerCount]
-     */
-    val startNewGameButton = Button(
-        posX = ((1920/2)-100),
-        posY = (1080-150),
+    val delayInputPlayer1: TextField = TextField(
+        posX = ((1920 / 2) - 100),
+        posY = (1080 - 250),
         height = 50,
-        width = 200,
-        text = "Start New Game",
-        alignment = Alignment.CENTER,
-        visual = ColorVisual(Color.YELLOW)
-    ).apply {
-        onMouseClicked = {
-            rootService.gameService.startNewGame(getPlayerList(testCheck.isSelected))
+        width = 50,
+        text = "5",
+    )
+    private fun toggleDelayInputVisibility(visibility: Boolean): Unit {
+
+    }
+
+
+        /**
+         * Creates a TextField were the User can Input a PlayerName
+         * Length of the input is unrestricted
+         * @param textInput String that gets put into text of the TextField
+         */
+        private fun createPlayerInputfield(textInput: String): TextField {
+            val textField = TextField(
+                posY = 0,
+                posX = 100,
+                width = 300,
+                height = 50,
+                font = Font(20, Color.BLACK),
+                text = textInput,
+            ).apply {
+                /*Check if Startnewgame should be disabled with every Keypress
+            * StartNewGameButton is Enabled from the Start so Going with Default Names works*/
+                onKeyTyped = {
+                    if (determinePlayerCount() < 2 || determinePlayerCount() > 5) {
+                        startNewGameButton.isDisabled = true
+                    } else {
+                        startNewGameButton.isDisabled = false
+                    }
+                    //Limit for InputLength could be added here
+                }
+            }
+
+            return textField
+        }
+
+
+        /**
+         * creates a Button that onCLick adds Inputfield TypeSelector and Remove Button to a Row in the grid
+         * does the Opposite as RemovePlayerButton
+         * @return Button
+         */
+        private fun createPlayerAddButton(position: Int): Button {
+            val addButton = Button(
+                posX = 100,
+                posY = 0,
+                width = 50,
+                height = 50,
+                text = "add",
+                alignment = Alignment.CENTER,
+                //visual = ImageVisual("Test.png")
+            ).apply {
+                onMouseClicked = {
+                    playerGrid[0, position] = createPlayerInputfield("Player${position + 1}")
+                    playerGrid[1, position] = createPlayerTypeSelector(position)
+                    playerGrid[2, position] = createRemovePlayerButton(position)
+                }
+            }
+            return addButton
+        }
+
+        /**
+         * creates a button that will remove Player Creation Buttons from the grid at its position
+         * will also delete itself
+         * @return button
+         */
+        private fun createRemovePlayerButton(position: Int): Button {
+            val removeButton = Button(
+                posX = 0,
+                posY = 0,
+                width = 50,
+                height = 50,
+                text = "remove",
+                alignment = Alignment.CENTER_LEFT,
+                //visual = ImageVisual("Test.png"),
+            ).apply {
+                onMouseClicked = {
+                    //remove
+                    playerGrid[0, position] = createPlayerAddButton(position)
+                    playerGrid[1, position] = null
+                    playerGrid[2, position] = null
+
+                    if (determinePlayerCount() >= 2) {
+                        startNewGameButton.isDisabled = true
+                    }
+                }
+            }
+            return removeButton
+        }
+
+        /**
+         * creates a Button that can switch from one Playertype to another
+         * State is saved in text with Fontsize 0
+         * @return Button
+         */
+        private fun createPlayerTypeSelector(position: Int): Button {
+            val typeSelector = Button(
+                posX = 0,
+                posY = 0,
+                width = 50,
+                height = 50,
+                text = "PLAYER",
+                font = Font(size = 0),
+                alignment = Alignment.CENTER,
+                //visual = ImageVisual("PLAYER.png"),
+            ).apply {
+                /*Unsure if we should be able to swap a Player to a Network Player
+            * Network case can be removed in that case*/
+                onMouseClicked = {
+                    when (text) {
+                        "PLAYER" -> {
+                            text = "AI";
+                            //visual = ImageVisual("AI.png");
+                            //delayInputPlayer1.apply { delayInputPlayer1.isVisible = true }
+                        }
+
+                        "AI" -> {
+                            text = "RANDOM";
+                            //visual = ImageVisual("RANDOM.png");
+                            //delayInputPlayer1.apply { delayInputPlayer1.isVisible = true }
+                        }
+
+                        "RANDOM" -> {
+                            text = "NETWORK";
+                            //visual = ImageVisual("NETWORK.png");
+                            //delayInputPlayer1.apply { delayInputPlayer1.isVisible = false }
+                        }
+
+                        "NETWORK" -> {
+                            text = "PLAYER";
+                            //visual = ImageVisual("PLAYER.png")
+                            //delayInputPlayer1.apply { delayInputPlayer1.isVisible = false }
+                        }
+
+                        else -> {
+                            text = "PLAYER";
+                            //visual = ImageVisual("PLAYER.png")
+                            //delayInputPlayer1.apply { delayInputPlayer1.isVisible = false }
+                        }
+                    }
+                }
+            }
+            return typeSelector
+        }
+
+        /**This Button Needs
+         * a [getPlayerList] Function
+         * to validate amount of Players with Names [determinePlayerCount]
+         */
+        val startNewGameButton = Button(
+            posX = ((1920 / 2) - 100),
+            posY = (1080 - 150),
+            height = 50,
+            width = 200,
+            text = "Start New Game",
+            alignment = Alignment.CENTER,
+            visual = ColorVisual(Color.YELLOW)
+        ).apply {
+            onMouseClicked = {
+                rootService.gameService.startNewGame(getPlayerList(testCheck.isSelected))
+            }
+        }
+
+        /**
+         * Determines the PlayerType of the Playertype Button
+         * @param button that needs to be determined
+         * @return PlayerType of button
+         */
+        private fun getPlayerType(button: UIComponent?): PlayerType {
+            var selectedPlayerType: PlayerType = PlayerType.PLAYER
+            if (button is Button) {
+                when (button.text) {
+                    "PLAYER" -> selectedPlayerType = PlayerType.PLAYER
+                    "AI" -> selectedPlayerType = PlayerType.AI
+                    "RANDOM" -> selectedPlayerType = PlayerType.RANDOM_AI
+                    "NETWORK" -> selectedPlayerType = PlayerType.NETWORK
+                    else -> selectedPlayerType = PlayerType.PLAYER
+                }
+            }
+            return selectedPlayerType
+        }
+
+        /**
+         *  Creates a List of Players [playerList] for the Start game button
+         *  DONE: needs to acutally shuffle the List if [random] == true
+         *  @return List with <Player>
+         */
+        private fun getPlayerList(random: Boolean): MutableList<Pair<String, PlayerType>> {
+            val playerList: MutableList<Pair<String, PlayerType>> = mutableListOf()
+            for (i in 0 until playerGrid.rows) {
+                val current = if (playerGrid[0, i] != null) {
+                    playerGrid[0, i]
+                } else {
+                    continue
+                }
+                if (current is TextField && current.text.isNotEmpty()) {
+                    playerList.add(Pair(current.text, getPlayerType(playerGrid[1, i])))
+                }
+            }
+            if (random) {
+                playerList.shuffle()
+            }
+            return playerList
+        }
+
+        /**
+         * gives the amount of Players currently set by the User
+         * @retun an Int Value
+         */
+        private fun determinePlayerCount(): Int {
+            var playerCount: Int = 0
+            for (i in 0 until playerGrid.rows) {
+                val current = if (playerGrid[0, i] != null) {
+                    playerGrid[0, i]
+                } else {
+                    continue
+                }
+                if (current is TextField && current.text.isNotEmpty()) {
+                    playerCount++
+                }
+            }
+            return playerCount
+        }
+
+        init {
+            addComponents(
+                testCheck,
+                playerGrid,
+                startNewGameButton,
+                delayInputPlayer1,
+
+            )
+            playerGrid.setColumnWidth(0, 300)
+            playerGrid.setColumnWidth(1, 50)
+            playerGrid.setColumnWidth(2, 50)
+            playerGrid.setCenterMode(Alignment.CENTER)
+
+
+            playerGrid[0, 0] = createPlayerInputfield("Player1")
+            playerGrid[0, 1] = createPlayerInputfield("Player2")
+            playerGrid[1, 0] = createPlayerTypeSelector(0)
+            playerGrid[1, 1] = createPlayerTypeSelector(1)
+            playerGrid[0, 2] = createPlayerAddButton(2)
+            playerGrid[0, 3] = createPlayerAddButton(3)
+            playerGrid[0, 4] = createPlayerAddButton(4)
         }
     }
 
-    /**
-     * Determines the PlayerType of the Playertype Button
-     * @param button that needs to be determined
-     * @return PlayerType of button
-     */
-    private fun getPlayerType(button : UIComponent?) : PlayerType{
-        var selectedPlayerType: PlayerType = PlayerType.PLAYER
-        if(button is Button) {
-            when (button.text) {
-                "PLAYER" -> selectedPlayerType = PlayerType.PLAYER
-                "AI" -> selectedPlayerType = PlayerType.AI
-                "RANDOM" -> selectedPlayerType = PlayerType.RANDOM_AI
-                "NETWORK" -> selectedPlayerType = PlayerType.NETWORK
-                else -> selectedPlayerType = PlayerType.PLAYER
-            }
-        }
-        return selectedPlayerType
-    }
-
-    /**
-     *  Creates a List of Players [playerList] for the Start game button
-     *  DONE: needs to acutally shuffle the List if [random] == true
-     *  @return List with <Player>
-     */
-    private fun getPlayerList(random : Boolean) : MutableList<Pair<String,PlayerType>> {
-        val playerList: MutableList<Pair<String,PlayerType>> = mutableListOf()
-        for (i in 0 until playerGrid.rows) {
-            val current = if (playerGrid[0, i] != null) {
-                playerGrid[0, i]
-            } else {
-                continue
-            }
-            if (current is TextField && current.text.isNotEmpty() ) {
-                playerList.add( Pair( current.text , getPlayerType( playerGrid[1,i] )))
-            }
-        }
-        if( random ) { playerList.shuffle() }
-        return playerList
-    }
-
-    /**
-     * gives the amount of Players currently set by the User
-     * @retun an Int Value
-     */
-    private fun determinePlayerCount() : Int{
-        var playerCount : Int = 0
-        for ( i in 0 until playerGrid.rows)
-        {
-            val current = if(playerGrid[0,i] != null)
-            {
-                playerGrid[0,i]
-            }else
-            {
-                continue
-            }
-            if(current is TextField && current.text.isNotEmpty())
-            {
-                playerCount++
-            }
-        }
-        return playerCount
-    }
-
-    init {
-        addComponents(
-            testCheck,
-            playerGrid,
-            startNewGameButton,
-        )
-        playerGrid.setColumnWidth(0, 300)
-        playerGrid.setColumnWidth(1, 50)
-        playerGrid.setColumnWidth(2, 50)
-        playerGrid.setCenterMode(Alignment.CENTER)
-
-
-        playerGrid[0,0] = createPlayerInputfield("Player1")
-        playerGrid[0,1] = createPlayerInputfield("Player2")
-        playerGrid[1,0] = createPlayerTypeSelector()
-        playerGrid[1,1] = createPlayerTypeSelector()
-        playerGrid[0,2] = createPlayerAddButton(2)
-        playerGrid[0,3] = createPlayerAddButton(3)
-        playerGrid[0,4] = createPlayerAddButton(4)
-    }
-}
 
 /*Fails to build in Bgw? :(((*/
 fun main() {
